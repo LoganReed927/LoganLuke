e36b8c465b604058733f97d78afa7df2b9af7bf6
ewanlp <ewanlp@gmail.com>
2018-02-22 09:33:43 -0700

    updating views

---
+++ fuel/app/logs/2018/02/17.php
@@ -0,0 +1,8 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2018-02-17 07:17:28 --> Parsing Error - syntax error, unexpected '::' (T_PAAMAYIM_NEKUDOTAYIM) in /s/bach/c/under/ewanlp/fuel/app/classes/controller/demo.php on line 28
+ERROR - 2018-02-17 07:18:27 --> Error - The requested view could not be found: nav.php in /s/bach/c/under/ewanlp/fuel/core/classes/view.php on line 440
+ERROR - 2018-02-17 07:20:22 --> Error - The requested view could not be found: nav.php in /s/bach/c/under/ewanlp/fuel/core/classes/view.php on line 440
+ERROR - 2018-02-17 07:20:23 --> Error - The requested view could not be found: nav.php in /s/bach/c/under/ewanlp/fuel/core/classes/view.php on line 440
+ERROR - 2018-02-17 07:25:15 --> Notice - Undefined variable: content in /s/bach/c/under/ewanlp/fuel/app/views/demo/layoutfull.php on line 51
+ERROR - 2018-02-17 07:26:02 --> Notice - Undefined variable: content in /s/bach/c/under/ewanlp/fuel/app/views/demo/layoutfull.php on line 15
---
+++ fuel/app/logs/2018/02/21.php
@@ -0,0 +1,11 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2018-02-21 14:44:19 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 14:44:31 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 14:46:11 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 14:47:29 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 14:48:34 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 14:48:42 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 15:21:39 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 15:23:18 --> Compile Error - Cannot redeclare class Controller_Demo in /s/bach/c/under/ewanlp/fuel/app/classes/controller/test.php on line 35
+ERROR - 2018-02-21 15:33:33 --> Error - The requested view could not be found: nebraska/index.php in /s/bach/c/under/ewanlp/fuel/core/classes/view.php on line 440
