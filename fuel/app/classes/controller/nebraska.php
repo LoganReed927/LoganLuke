000a9a67be289598eb6e03a4085f5d929532ec89
ewanlp <ewanlp@gmail.com>
2018-02-24 06:24:34 -0700

    added more images and made all the components use fuel layouts and such

--- fuel/app/classes/controller/nebraska.php
+++ fuel/app/classes/controller/nebraska.php
@@ -6,6 +6,15 @@ class Controller_Nebraska extends Controller{
     public function action_index()
     {
         $layout = View::forge('nebraska/index');//loads template for page
+        $nav = View::forge('nebraska/nav');
+        $carosel = View::forge('nebraska/carosel');
+        $places = View::forge('nebraska/places');
+        $footer = View::forge('nebraska/footer');
+
+        $layout->nav = Response::forge($nav);
+        $layout->carosel = Response::forge($carosel);
+        $layout->places = Response::forge($places);
+        $layout->footer = Response::forge($footer);
         return $layout;
     }
 }
--- fuel/app/views/nebraska/index.php
+++ fuel/app/views/nebraska/index.php
@@ -12,68 +12,22 @@
 </head>
 <body>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Nebraska</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<!-- Start of the navigation bar-->
+  <?=$nav; ?>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Attractions
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Chimney Rock</a>
-            <a class="dropdown-item" href="#">Carhenge</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+<!-- The start of the changing images -->
+  <div class="container" id="top">
+    <?=$carosel; ?>
+  </div>
+<br /><br />
+
+<!-- The start of the Welcome to part -->
+  <div class="container">
+    <?=$places; ?>
+  </div>
 
   <div class="container">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="./../img/sign.jpg" alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Hiking!</h5>
-              <p>Hiked in the winter, Mt. Belford</p>
-            </div>
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="./../img/sign.jpg" alt="Second slide">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Hiking!</h5>
-            <p>Early Spring, Mt. Humboltd</p>
-          </div>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
+    <?= $footer; ?>
   </div>
 
 
