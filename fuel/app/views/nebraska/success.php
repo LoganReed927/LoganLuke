10f9ecabdce3d428e1bf2fc2104e696b74da6b30
Logan Reed <lvreed@rams.colostate.edu>
2018-03-04 12:43:11 -0700

    Update success.php
    changed success url to homepage instead of carhenge

--- fuel/app/views/nebraska/success.php
+++ fuel/app/views/nebraska/success.php
@@ -1,5 +1,5 @@
 <?php if($status === 'success') {
-  header('location: http://www.cs.colostate.edu/~ewanlp/ct310/index.php/nebraska/carhenge');
+  header('location: http://www.cs.colostate.edu/~ewanlp/ct310/index.php/nebraska');
 }else {
   header('location: http://www.cs.colostate.edu/~ewanlp/ct310/index.php/nebraska/loginError');
 } ?>
